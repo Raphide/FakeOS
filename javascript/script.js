3186fcff44e1430d618ab043f4e309105c29609b
Raphide <atlas.dino.usic@outlook.com>
2024-06-28 16:21:59 +1000

    added actual main branch

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+const home = document.getElementById("home");
+const calcapp = document.getElementById("app app--calc");
+const calculator = document.querySelector(".calculator");
+const clock = document.getElementById("clock");
+
+/// Clock
+function nowTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const clockStr = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+
+  const clockAppStr = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+  clock.innerText = clockStr;
+}
+nowTime();
+setInterval(nowTime, 1000);
+///===========================================
+
+///App buttons===============================
+home.addEventListener("click", () => {
+  calculator.classList.remove("show");
+});
+
+calcapp.addEventListener("click", () => {
+  calculator.classList.add("show");
+});
+///===============================
+
