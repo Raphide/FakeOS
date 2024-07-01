482d3a23a03b1bc904251159484bd543b4dacc91
Raphide <atlas.dino.usic@outlook.com>
2024-07-02 01:19:28 +1000

    finalising develop build

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,7 @@
 const home = document.getElementById("home");
 const calcapp = document.getElementById("app app--calc");
 const calculator = document.querySelector(".calculator");
-const clock = document.getElementById("clockTop");
 const clockMod = document.querySelector(".clock");
-const clockDisp = document.getElementById("clockdisplay");
 const clockApp = document.getElementById("app app--clock");
 const notes = document.querySelector(".notes");
 const notesApp = document.getElementById("app app--notes");
@@ -12,28 +10,6 @@ const phoneApp = document.getElementById("app app--phonebook");
 const list = document.getElementById("appslist");
 const listApp = document.getElementById("app app--list");
 const lock = document.querySelector(".lock");
-
-/// Clock
-function nowTime() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  const clockStr = `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}`;
-
-  const clockAppStr = `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-
-  clock.innerText = clockStr;
-  clockDisp.innerText = clockAppStr;
-}
-nowTime();
-setInterval(nowTime, 1000);
-///===========================================
 
 ///App buttons===============================
 home.addEventListener("click", () => {
@@ -47,26 +23,23 @@ calcapp.addEventListener("click", () => {
 });
 
 notesApp.addEventListener("click", () => {
-notes.classList.add("show");
+  notes.classList.add("show");
 });
-
 
 clockApp.addEventListener("click", () => {
   clockMod.classList.add("show");
 });
 ///===============================
- // menu buttons================================
- phoneApp.addEventListener("click", () => {
+// menu buttons================================
+phoneApp.addEventListener("click", () => {
   phone.classList.toggle("show");
- });
+});
 
- listApp.addEventListener("click", () => {
+listApp.addEventListener("click", () => {
   list.classList.toggle("show");
- })
+});
 
- //Lock screen===================================
+//Lock screen===================================
 lock.addEventListener("click", () => {
-  lock.classList.add("display-none")
-})
-
-
+  lock.classList.add("hide");
+});
